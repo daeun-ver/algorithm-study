bea3765feb601faf761cbac0131c63dcf5708381
daeun-ver <kyhee2001@naver.com>
2026-03-12 19:59:46 +0900

    solve: programmers basic 문자열 출력하기

---
+++ app/src/main/java/programmers/step02_basic/p002_println.kt
@@ -0,0 +1,9 @@
+package programmers.step02_basic
+
+//문자열 str이 주어질 때, str을 출력하는 코드를 작성해 보세요.
+class p002_println {
+    fun main(args: Array<String>) {
+        val s1 = readLine()!!
+        println(s1)
+    }
+}
