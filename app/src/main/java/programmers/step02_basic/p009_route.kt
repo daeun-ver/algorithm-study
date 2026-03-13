ee937a3117ab93836689d5cd380ba66c34293015
daeun-ver <kyhee2001@naver.com>
2026-03-13 19:35:50 +0900

    solve: programmers basic 문자열 돌리기

---
+++ app/src/main/java/programmers/step02_basic/p009_route.kt
@@ -0,0 +1,13 @@
+package programmers.step02_basic
+
+//문자열 str이 주어집니다.
+//문자열을 시계방향으로 90도 돌려서 아래 입출력 예와 같이 출력하는 코드를 작성해 보세요.
+
+class p009_route {
+    fun main(args: Array<String>) {
+        val s1 = readLine()!!
+        for(c in s1){
+            println(c)
+        }
+    }
+}
