c1b64bd5c357a22aae9f57f1bd26050fe9c1be10
daeun-ver <kyhee2001@naver.com>
2026-03-13 19:25:15 +0900

    solve: programmers basic 특수문자 출력하기

---
+++ app/src/main/java/programmers/step02_basic/p006_print_special_char.kt
@@ -0,0 +1,9 @@
+package programmers.step02_basic
+
+//다음과 같이 출력하도록 코드를 작성해 주세요.
+
+class p006_print_special_char {
+    fun main(args: Array<String>) {
+        println("""!@#$%^&*(\'"<>?:;""")
+    }
+}
