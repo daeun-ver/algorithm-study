40168ce4959908a4348590a408f8fa26ce01af7a
daeun-ver <kyhee2001@naver.com>
2026-03-26 23:48:00 +0900

    solve: programmers basic 카운트 업

---
+++ app/src/main/java/programmers/step02_basic/p027_count_up.kt
@@ -0,0 +1,13 @@
+package programmers.step02_basic
+
+//정수 start_num와 end_num가 주어질 때, start_num부터 end_num까지의 숫자를 차례로 담은 리스트를 return
+
+class p027_count_up {
+    fun solution(start_num: Int, end_num: Int): IntArray {
+        var answer = mutableListOf<Int>()
+        for (i in start_num..end_num) {
+            answer.add(i)
+        }
+        return answer.toIntArray()
+    }
+}
