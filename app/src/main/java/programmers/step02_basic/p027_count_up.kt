ed5166ec30a18774fd7896daa738ea26d1843be3
daeun-ver <kyhee2001@naver.com>
2026-03-26 23:48:00 +0900

    solve: programmers basic 카운트 업

--- app/src/main/java/programmers/step02_basic/p027_count_up.kt
+++ app/src/main/java/programmers/step02_basic/p027_count_up.kt
@@ -10,4 +10,12 @@ class p027_count_up {
         }
         return answer.toIntArray()
     }
+
+//    fun solution(start: Int, end: Int): IntArray {
+//        var answer: IntArray = intArrayOf()
+//        for (i in start..end) {
+//            answer += i
+//        }
+//        return answer
+//    }
 }
