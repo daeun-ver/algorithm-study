da7a4a151a60b468d49ef5c3f3af601f7b881752
daeun-ver <kyhee2001@naver.com>
2026-03-27 13:11:40 +0900

    solve: programmers basic 문자열 뒤의 n글자

--- app/src/main/java/programmers/step02_basic/p031_back_string.kt
+++ app/src/main/java/programmers/step02_basic/p031_back_string.kt
@@ -1,4 +1,10 @@
 package programmers.step02_basic
 
+//문자열 my_string과 정수 n이 매개변수로 주어질 때, my_string의 뒤의 n글자로 이루어진 문자열을 return
+
 class p031_back_string {
+    fun solution(my_string: String, n: Int): String {
+
+        return my_string.takeLast(n)
+    }
 }
