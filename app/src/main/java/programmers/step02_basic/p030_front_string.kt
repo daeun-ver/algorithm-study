ac66ca3428fc5b7637276494155671786e61fd94
daeun-ver <kyhee2001@naver.com>
2026-03-27 13:09:00 +0900

    solve: programmers basic 문자열 앞의 n글자

---
+++ app/src/main/java/programmers/step02_basic/p030_front_string.kt
@@ -0,0 +1,13 @@
+package programmers.step02_basic
+
+//문자열 my_string과 정수 n이 매개변수로 주어질 때, my_string의 앞의 n글자로 이루어진 문자열을 return
+
+class p030_front_string {
+    fun solution(my_string: String, n: Int): String {
+        var answer: String = ""
+        for (i in 0 until n) {
+            answer += my_string[i]
+        }
+        return answer
+    }
+}
