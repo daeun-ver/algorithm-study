3648577525b28389e98fa8ba32f9faebfa6f6045
daeun-ver <kyhee2001@naver.com>
2026-04-15 23:51:08 +0900

    solve: programmers basic 대문자로 바꾸기

---
+++ app/src/main/java/programmers/step02_basic/p053_uppercase.kt
@@ -0,0 +1,10 @@
+package programmers.step02_basic
+
+//알파벳으로 이루어진 문자열 myString이 주어집니다. 모든 알파벳을 대문자로 변환하여 return
+
+class p053_uppercase {
+    fun solution(myString: String): String {
+
+        return myString.uppercase()
+    }
+}
