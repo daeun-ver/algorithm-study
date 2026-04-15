ad59c823788de3be8e491140af79f7c585ecab46
daeun-ver <kyhee2001@naver.com>
2026-04-15 23:52:17 +0900

    solve: programmers basic 소문자로 바꾸기

---
+++ app/src/main/java/programmers/step02_basic/p054_lowercase.kt
@@ -0,0 +1,10 @@
+package programmers.step02_basic
+
+//알파벳으로 이루어진 문자열 myString이 주어집니다. 모든 알파벳을 소문자로 변환하여 return
+
+class p054_lowercase {
+    fun solution(myString: String): String {
+
+        return myString.lowercase()
+    }
+}
