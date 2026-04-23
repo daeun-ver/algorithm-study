db15682f65cc71a47e81f2e31a5da3141086bdbf
daeun-ver <kyhee2001@naver.com>
2026-04-24 02:08:34 +0900

    solve: programmers basic 뒤에서 5등까지

--- app/src/main/java/programmers/step02_basic/p061_rank_5.kt
+++ app/src/main/java/programmers/step02_basic/p061_rank_5.kt
@@ -1,4 +1,10 @@
 package programmers.step02_basic
 
+//정수로 이루어진 리스트 num_list가 주어집니다. num_list에서 가장 작은 5개의 수를 오름차순으로 담은 리스트를 return
+
 class p061_rank_5 {
+    fun solution(num_list: IntArray): IntArray {
+
+        return num_list.sorted().take(5).toIntArray()
+    }
 }
