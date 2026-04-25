46148e661b3809e90c0a097c5efbf072173fa7ca
daeun-ver <kyhee2001@naver.com>
2026-04-25 22:27:28 +0900

    solve: programmers basic 문자열을 정수로 변환하기

---
+++ app/src/main/java/programmers/step02_basic/p063_to_Int.kt
@@ -0,0 +1,9 @@
+package programmers.step02_basic
+
+//숫자로만 이루어진 문자열 n_str이 주어질 때, n_str을 정수로 변환하여 return
+
+class p063_to_Int {
+    fun solution(n_str: String): Int {
+        return n_str.toInt()
+    }
+}
