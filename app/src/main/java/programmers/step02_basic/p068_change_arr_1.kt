0aff73a1555d10e370ca06f4f9f4fae4cdc62216
daeun-ver <kyhee2001@naver.com>
2026-04-30 23:48:30 +0900

    solve: programmers basic 조건에 맞게 수열 변환하기 1

---
+++ app/src/main/java/programmers/step02_basic/p068_change_arr_1.kt
@@ -0,0 +1,19 @@
+package programmers.step02_basic
+
+//정수 배열 arr가 주어집니다. arr의 각 원소에 대해 값이 50보다 크거나 같은 짝수라면 2로 나누고, 50보다 작은 홀수라면 2를 곱합니다. 그 결과인 정수 배열을 return
+
+class p068_change_arr_1 {
+    fun solution(arr: IntArray): IntArray {
+        var answer: IntArray = intArrayOf()
+        for (i in arr) {
+            if (i >= 50 && i % 2 == 0) {
+                answer += i / 2
+            } else if (i < 50 && i % 2 == 1) {
+                answer += i * 2
+            } else {
+                answer += i
+            }
+        }
+        return answer
+    }
+}
