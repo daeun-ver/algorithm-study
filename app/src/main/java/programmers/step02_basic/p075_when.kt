b9e3dc87f436690c4d5166fc77553fbdc27c0af8
daeun-ver <kyhee2001@naver.com>
2026-05-08 23:50:33 +0900

    solve: programmers basic 길이에 따른 연산

---
+++ app/src/main/java/programmers/step02_basic/p075_when.kt
@@ -0,0 +1,20 @@
+package programmers.step02_basic
+
+//정수가 담긴 리스트 num_list가 주어질 때, 리스트의 길이가 11 이상이면 리스트에 있는 모든 원소의 합을 10 이하이면 모든 원소의 곱을 return
+
+class p075_when {
+    fun solution(num_list: IntArray): Int {
+        var answer: Int = 0
+        if (num_list.size >= 11) {
+            for (i in num_list) {
+                answer += i
+            }
+        } else {
+            answer = 1
+            for (i in num_list) {
+                answer *= i
+            }
+        }
+        return answer
+    }
+}
