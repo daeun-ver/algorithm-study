2a60907811e7184e9bcf921bdde86e6e29a11308
daeun-ver <kyhee2001@naver.com>
2026-05-18 00:36:59 +0900

    solve: programmers basic 문자열로 변환

---
+++ app/src/main/java/programmers/step02_basic/p086_toString.kt
@@ -0,0 +1,10 @@
+package programmers.step02_basic
+
+//정수 n이 주어질 때, n을 문자열로 변환하여 return하도록 solution 함수를 완성해주세요.
+
+class p086_toString {
+    fun solution(n: Int): String {
+
+        return n.toString()
+    }
+}
