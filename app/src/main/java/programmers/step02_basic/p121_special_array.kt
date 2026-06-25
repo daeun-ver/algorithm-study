337ebb4cc32e172ae12290c2dd563f96f78625c9
daeun-ver <kyhee2001@naver.com>
2026-06-25 20:36:26 +0900

    solve: programmers basic 특별한 이차원 배열 1

---
+++ app/src/main/java/programmers/step02_basic/p121_special_array.kt
@@ -0,0 +1,19 @@
+package programmers.step02_basic
+
+//정수 n이 매개변수로 주어질 때, 다음과 같은 n × n 크기의 이차원 배열 arr를 return
+//arr[i][j] (0 ≤ i, j < n)의 값은 i = j라면 1, 아니라면 0입니다.
+
+class p121_special_array {
+    fun solution(n: Int): Array<IntArray> {
+        val answer = Array(n) { IntArray(n) }
+
+        for (i in 0 until n) {
+            for (j in 0 until n) {
+                if (i == j) {
+                    answer[i][j] = 1
+                }
+            }
+        }
+        return answer
+    }
+}
