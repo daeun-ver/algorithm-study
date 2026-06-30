577f474eb39de6c60f9ac886fbcae66ff15392b4
daeun-ver <kyhee2001@naver.com>
2026-07-01 00:03:42 +0900

    solve: programmers beginner 문자열을 정수로 바꾸기

---
+++ app/src/main/java/programmers/step03_beginner/p02_string_to_Int.kt
@@ -0,0 +1,10 @@
+package programmers.step03_beginner
+
+//문자열 s를 숫자로 변환한 결과를 반환하는 함수, solution을 완성하세요.
+
+class p02_string_to_Int {
+    fun solution(s: String): Int {
+
+        return s.toInt()
+    }
+}
