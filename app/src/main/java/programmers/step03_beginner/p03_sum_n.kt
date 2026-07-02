2004640fd4b7eb266670b08bf7995ce4f6d8821e
daeun-ver <kyhee2001@naver.com>
2026-07-02 23:27:30 +0900

    solve: programmers beginner 약수의 합

---
+++ app/src/main/java/programmers/step03_beginner/p03_sum_n.kt
@@ -0,0 +1,15 @@
+package programmers.step03_beginner
+
+//정수 n을 입력받아 n의 약수를 모두 더한 값을 리턴하는 함수, solution을 완성해주세요.
+
+class p03_sum_n {
+    fun solution(n: Int): Int {
+        var answer = 0
+        for (i in 1..n) {
+            if (n % i == 0) {
+                answer += i
+            }
+        }
+        return answer
+    }
+}
