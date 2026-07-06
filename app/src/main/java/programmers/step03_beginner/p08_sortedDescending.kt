f1960b5bbef0c604615e52d0c7d5b9e4ff325bfd
daeun-ver <kyhee2001@naver.com>
2026-07-07 00:55:55 +0900

    solve: programmers beginner 정수 내림차순으로 배치하기

---
+++ app/src/main/java/programmers/step03_beginner/p08_sortedDescending.kt
@@ -0,0 +1,11 @@
+package programmers.step03_beginner
+
+//함수 solution은 정수 n을 매개변수로 입력받습니다. n의 각 자릿수를 큰것부터 작은 순으로 정렬한 새로운 정수를 리턴해주세요. 예를들어 n이 118372면 873211을 리턴하면 됩니다.
+
+class p08_sortedDescending {
+    fun solution(n: Long): Long {
+        var answer: Long = 0
+
+        return n.toString().toList().sortedDescending().joinToString("").toLong()
+    }
+}
