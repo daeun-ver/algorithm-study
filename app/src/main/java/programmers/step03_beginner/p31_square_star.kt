e70f545b64f0472dedbd836e47bd1829f9714dcc
daeun-ver <kyhee2001@naver.com>
2026-08-02 23:47:23 +0900

    solve: programmers beginner 직사각형 별찍기

---
+++ app/src/main/java/programmers/step03_beginner/p31_square_star.kt
@@ -0,0 +1,16 @@
+package programmers.step03_beginner
+
+//이 문제에는 표준 입력으로 두 개의 정수 n과 m이 주어집니다.
+//별(*) 문자를 이용해 가로의 길이가 n, 세로의 길이가 m인 직사각형 형태를 출력해보세요.
+
+class p31_square_star {
+    fun main(args: Array<String>) {
+        val (a, b) = readLine()!!.split(' ').map(String::toInt)
+        for (i in 1..b) {
+            for (j in 1..a) {
+                print("*")
+            }
+            println()
+        }
+    }
+}
