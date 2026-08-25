a54b0bd017000dc0e223209e1be45bc90764d9e0
daeun-ver <kyhee2001@naver.com>
2026-08-25 23:51:35 +0900

    solve: programmers beginner 카드 뭉치

---
+++ app/src/main/java/programmers/step03_beginner/p45_firstOrNull.kt
@@ -0,0 +1,20 @@
+package programmers.step03_beginner
+
+//문자열로 이루어진 배열 cards1, cards2와 원하는 단어 배열 goal이 매개변수로 주어질 때,
+//cards1과 cards2에 적힌 단어들로 goal를 만들 있다면 "Yes"를, 만들 수 없다면 "No"를 return하는 solution 함수를 완성해주세요.
+class p45_firstOrNull {
+    fun solution(cards1: Array<String>, cards2: Array<String>, goal: Array<String>): String {
+        val list1 = cards1.toMutableList()
+        val list2 = cards2.toMutableList()
+        for (word in goal) {
+            if (list1.firstOrNull() == word) {
+                list1.removeAt(0)
+            } else if (list2.firstOrNull() == word) {
+                list2.removeAt(0)
+            } else {
+                return "No"
+            }
+        }
+        return "Yes"
+    }
+}
