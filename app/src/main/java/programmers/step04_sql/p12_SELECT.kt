aa0a7c1955b060ca4ca25017df19c9e805544ced
daeun-ver <kyhee2001@naver.com>
2026-09-23 00:02:12 +0900

    solve: programmers sql 어린 동물 찾기

---
+++ app/src/main/java/programmers/step04_sql/p12_SELECT.kt
@@ -0,0 +1,20 @@
+package programmers.step04_sql
+
+/*
+ * [테이블 구조]
+ * - ANIMAL_ID        | VARCHAR(N)  | FALSE | 동물 ID (PK)
+ * - ANIMAL_TYPE      | VARCHAR(N)  | FALSE | 생물 종
+ * - DATETIME         | DATETIME    | FALSE | 보호 시작일
+ * - INTAKE_CONDITION | VARCHAR(N)  | FALSE | 상태 (Normal, Sick 등)
+ * - NAME             | VARCHAR(N)  | TRUE  | 이름 (NULL 허용)
+ * - SEX_UPON_INTAKE  | VARCHAR(N)  | FALSE | 성별 및 중성화 여부
+ *
+ * 동물 보호소에 들어온 동물 중 젊은 동물의 아이디와 이름을 조회하는 SQL 문을 작성해주세요. 이때 결과는 아이디 순으로 조회해주세요.
+ */
+
+val query12 = """
+    SELECT ANIMAL_ID, NAME
+    FROM ANIMAL_INS
+    WHERE INTAKE_CONDITION != 'Aged'
+    ORDER BY ANIMAL_ID;
+    """.trimIndent()
